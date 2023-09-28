d6291dd4dd66ec64157b97fb9b4f7cfb0421d2ae
Nibha Maharjan <nibhamaharjan@yahoo.com>
2023-09-28 13:49:59 -0400

    Task 2 initialize

---
+++ NibhaMaharjan_MAPD711_Assign1/NibhaMaharjan_MAPD711_Ex2/app/src/main/java/com/example/nibhamaharjan_mapd711_ex2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nibhamaharjan_mapd711_ex2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
